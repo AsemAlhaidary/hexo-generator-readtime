224460e8078b12497645970012574310da8c3a34
ASEM F. ALHAIDARI <96296311+AsemAlhaidary@users.noreply.github.com>
2023-02-16 23:24:14 +0300

    Refactoring the code and using a default values for the package

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 'use strict';
 
-let logic = require('./lib/logic');
+let readTimeSettings = require('./Settings/_readTime.json');
 
 hexo.config.readtime = Object.assign({
   fields: ['content'],
-  wordsPerMinute: 15,
-  imgsPreMinute: 6,
+  humanReadable: true,
+  imgReadTime: 12,
+  countLangProfile: {
+    ar: {
+      charPerMin: 612,
+      wordsPerMin: 138,
+    },
+    en: {
+      charPerMin: 987,
+      wordsPerMin: 228,
+    },
+    jp: {
+      charPerMin: 357,
+      wordsPerMin: 193,
+    },
+    zh: {
+      charPerMin: 255,
+      wordsPerMin: 158,
+    },
+  },
 }, hexo.config.readtime);
 
 
-hexo.extend.filter.register('before_post_render', logic);
+hexo.extend.filter.register('before_post_render', (data) => require('./lib/logic')(data, hexo, readTimeSettings));
