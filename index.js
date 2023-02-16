dc9c21d426f2a51f806c7f13f26bba1a51c2aef4
ASEM F. ALHAIDARI <96296311+AsemAlhaidary@users.noreply.github.com>
2023-02-16 18:03:41 +0300

    Do refactoring to the code

--- index.js
+++ index.js
@@ -3,20 +3,9 @@
 let logic = require('./lib/logic');
 
 hexo.config.readtime = Object.assign({
-  wordCount: {
-    fields: ['contect'],
-  },
-  charCount: {
-    fields: ['content'],
-  },
-  imgCount: {
-    fields: ['content'],
-  },
-  readTime: {
-    fields: ['content'],
-    wordsPerMinute: 15,
-    imgsPreMinute: 6,
-  },
+  fields: ['content'],
+  wordsPerMinute: 15,
+  imgsPreMinute: 6,
 }, hexo.config.readtime);
 
 
