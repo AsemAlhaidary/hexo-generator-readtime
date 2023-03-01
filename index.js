7e896c231dd62382f5adc287475e88f713540302
LoreZyra <Rich@RichieBartlett.com>
2023-03-01 15:26:38 +0900

    Updating documentation and function names
    https://github.com/lorezyra/Richie2.com/issues/193
    Signed-off-by: LoreZyra <Rich@RichieBartlett.com>

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
+/* 
+ * Register the `before_post_render` event listener to trigger readTime
+ * See https://hexo.io/api/events for further details on events
+ */
+
 hexo.extend.filter.register('before_post_render', (data) => require('./lib/logic')(data, hexo, require('./Settings/_readTime.json')));
